a0113723f47b64921d3e384eca3347a226725946
J-Wengler <jwengler@gmail.com>
2021-02-17 07:22:18 -0700

    Removed old bash scripts

--- analysis/Bash_Scripts/getConceptVecs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILE=/Data/concept_model.bin
-
-if [ ! -f "$FILE" ]; then
-    wget -O ${FILE} https://ftp.ncbi.nlm.nih.gov/pub/lu/BioConceptVec/bioconceptvec_fasttext.bin
-fi    
--- analysis/Bash_Scripts/trainBERT.sh
+++
@@ -1,1 +0,0 @@
-python -m spacy pretrain ./raw_text.jsonl en_vectors_web_lg ./pretrained-model

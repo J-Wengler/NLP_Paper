f8326eee01aebbe77464815ab3f87726fc56e3ca
J-Wengler <jwengler@gmail.com>
2021-03-01 07:11:17 -0700

    Added GEO evaluation

--- analysis/Python_Scripts/geoAPI.py
+++ analysis/Python_Scripts/geoAPI.py
@@ -1,26 +1,32 @@
-path_to_GEO = "/Data/GEO_Queries/"
-path_to_queries = "/Data/Queries/"
+evaluateGEO()
 
-geo_results = []
-query_results = []
+def evaluateGEO():
+    path_to_GEO_queries = "/Data/GEO_Queries/"
+    path_to_queries = "/Data/Queries/"
 
-with open(path_to_GEO + "q4_acute_leukemia+mll+progression_GEO.txt", 'r') as geo_file:
-    for line in geo_file:
-        if line.startswith("Series"):
-            split_sent = line.split()
-            geo_results.append(split_sent[2])
+    geo_results = []
+    query_results = []
 
-with open(path_to_queries + "q4/names.txt", 'r') as query_file:
-    for line in query_file:
-        query_results = line.split()
+    query_list = ["q1_metastasis+brain_GEO.txt", "q2_sars_GEO.txt", "q3_h1n1+infection+mouse+lethal_GEO.txt"
+                  "q4_acute_leukemia+mll+progression_GEO.txt", "q5_BRCA+Cancer_GEO.txt",
+                  "q6_heart_development+age+failure_GEO.txt"]
+    for path in query_list:
+        with open(path_to_GEO_queries + path, 'r') as geo_file:
+            for line in geo_file:
+                if line.startswith("Series"):
+                    split_sent = line.split()
+                    geo_results.append(split_sent[2])
 
-num_relevant = 0
+        with open(path_to_queries + f"{path[1]}/names.txt", 'r') as query_file:
+            for line in query_file:
+                query_results = line.split()
 
-for series in geo_results:
-    if series in query_results:
-        num_relevant = num_relevant + 1
+        num_relevant = 0
+        for series in geo_results:
+            if series in query_results:
+                num_relevant = num_relevant + 1
 
+        resultsDirPath = f"/Data/Results/GEO_Queries/geo_results.txt"
+        with open (resultsDirPath, 'w+') as out_file:
+            out_file.write(f"q{path[1]} returned {num_relevant} in {len(query_results)}")
 
-print(f"Number relevant : {num_relevant}")
-print(f"Query_results : {len(query_results)}")
-print(f"Query 1 : {num_relevant / len(query_results)}")

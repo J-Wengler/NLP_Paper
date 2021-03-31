c66b2721fee6581606edac5c5183373efaee27fa
J-Wengler <jwengler@gmail.com>
2021-03-31 11:51:52 -0600

    .Join test completed

--- analysis/Python_Scripts/testMP.py
+++ analysis/Python_Scripts/testMP.py
@@ -3,35 +3,38 @@ import time
 
 def waitFive():
     time.sleep(5)
-    print("5 Seconds has passed since process 1 started")
+    print("5 seconds has passed since process 1 started")
 
 def waitTen():
     time.sleep(10)
     print("10 seconds has passed since process 2 started")
 
 
-five = multiprocessing.Process(target=waitFive())
-ten = multiprocessing.Process(target=waitTen())
+print()
+print("********** MP Tests **********")
 
-print("Using a for loop - .join() called directly after .start()")
-
-processes = [five, ten]
+print("Using a for loop simluation - .join() called directly after .start()")
 start = time.time()
-for pr in processes:
-    pr.start()
-    pr.join()
+five = multiprocessing.Process(target=waitFive)
+ten = multiprocessing.Process(target=waitTen)
+five.start()
+five.join()
+ten.start()
+ten.join()
 end = time.time()
 
-print('{:.4f} s total time'.format(end - start))
-
+print('{:.4f}s total time'.format(end - start))
+print()
 print("No loop - both .start() called before .join()")
 start = time.time()
+five = multiprocessing.Process(target=waitFive)
+ten = multiprocessing.Process(target=waitTen)
 five.start()
 ten.start()
 five.join()
 ten.join()
 end = time.time()
 
-print('{:.4f} s total time'.format(end - start))
-
+print('{:.4f}s total time'.format(end - start))
+print()
 

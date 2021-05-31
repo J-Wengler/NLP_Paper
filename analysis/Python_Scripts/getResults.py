c67c1c71cf7fc68143eb65ffb0a193d63e669352
J-Wengler <jwengler@gmail.com>
2021-05-31 11:03:47 -0600

    Fixed filepath

--- analysis/Python_Scripts/getResults.py
+++ analysis/Python_Scripts/getResults.py
@@ -3,7 +3,7 @@ from helper import *
 resultPath = ["MultipartitieRank", "PositionRank", "SingleRank", "TFIDF", "TextRank", "TopicalRank", "YAKE", "KPMINER", "TopicRankResults"]
 models = ["FastTextSkipGram", "FastTextCBOW", "FastTextWiki", "BIOWORDVEC", "SpacyWebLG", "SciSpacy"]
 numKeywords = [10,20,30]
-bestCombo = open('/Results/bestCombo.txt', 'w+')
+bestCombo = open('/home/jwengler/NLP_Paper/analysis/Data/Results/bestCombo.txt', 'w+')
 
 for model in models:
     for numKeyword in numKeywords:
